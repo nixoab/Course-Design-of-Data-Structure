88c39e991786c7704576ab902d49c630b00c351d
baoxin <baoxin@emails.bjut.edu.cn>
2021-11-12 16:11:33 +0800

    实现字频统计

--- huffmanTree.py
+++ huffmanTree.py
@@ -1,3 +1,11 @@
+def count_freq(text):
+    dictionary = {x:text.count(x) for x in text}
+    chars_freqs = []
+    for key, value in dictionary.items():
+        char_freq = (key, value)
+        chars_freqs.append(char_freq)
+    return chars_freqs
+
 #节点类
 class Node(object):
     def __init__(self,name = None, value = None):
@@ -23,17 +31,19 @@ class HuffmanTree(object):
     #用递归的思想生成编码
     def pre(self, tree, length):
         node = tree
+        s = ""
         if (not node):
             return
         elif node.name:
             print (node.name + '    encoding:', end = ''),
             for i in range(length):
-                print (self.Buffer[i], end = '')
-            print ('\n')
+                #print (self.Buffer[i], end = '')
+                s += self.Buffer[i]
+            print (s + '\n')
             return
-        self.Buffer[length] = 0
+        self.Buffer[length] = '0'
         self.pre(node.left, length + 1)
-        self.Buffer[length] = 1
+        self.Buffer[length] = '1'
         self.pre(node.right, length + 1)
     #生成哈夫曼编码   
     def get_code(self):
@@ -41,6 +51,8 @@ class HuffmanTree(object):
 
 if __name__=='__main__':
     #输入的是字符及其频数
-    char_weights = [(' ', 5), ('w', 4), ('l', 4), ('e', 2), ('i', 2), ('r',1), ('u', 1)]
+    text = "     wwwwlllleeiiru"
+    #char_weights = [(' ', 5), ('w', 4), ('l', 4), ('e', 2), ('i', 2), ('r',1), ('u', 1)]
+    char_weights = count_freq(text)
     tree = HuffmanTree(char_weights)
     tree.get_code()
