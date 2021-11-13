caafac839265473617558c897c30ba2138fda4e1
baoxin <baoxin@emails.bjut.edu.cn>
2021-11-13 15:57:58 +0800

    完成哈夫曼树

--- huffmanTree.py
+++ huffmanTree.py
@@ -1,11 +1,3 @@
-def count_freq(text):
-    dictionary = {x:text.count(x) for x in text}
-    chars_freqs = []
-    for key, value in dictionary.items():
-        char_freq = (key, value)
-        chars_freqs.append(char_freq)
-    return chars_freqs
-
 #节点类
 class Node(object):
     def __init__(self,name = None, value = None):
@@ -16,69 +8,74 @@ class Node(object):
 
 #哈夫曼树类
 class HuffmanTree(object):
-
-    #根据Huffman树的思想：以节点为基础，反向建立Huffman树
-    def __init__(self, char_weights):
-        self.Leav = [Node(part[0], part[1]) for part in char_weights]  #根据输入的字符及其频数生成节点
+    #初始化
+    def __init__(self, text):
+        self.text = text
+        self.chars_freqs = [] #字频
+        self.count_freq()
+        self.Leav = [Node(part[0], part[1]) for part in self.chars_freqs]  #根据输入的字符及其频数生成节点
+        self.creatTree()
+        self.root = self.Leav[0]   #根节点
+        self.curCode = [2] * 10000 #用于递归编码
+        self.codes = []            #编码表
+        self.huffmanStr = ""       #编码
+        self.originStr = ""        #解码
+    #字频统计
+    def count_freq(self):
+        dictionary = {c : self.text.count(c) for c in self.text}
+        for key, value in dictionary.items():
+            char_freq = (key, value)
+            self.chars_freqs.append(char_freq)
+    #建树
+    def creatTree(self):
         while len(self.Leav) != 1:
             self.Leav.sort(key = lambda node : node.value, reverse = True) #降序
-            c = Node(value = (self.Leav[-1].value + self.Leav[-2].value))
-            c.left = self.Leav.pop(-1)
-            c.right = self.Leav.pop(-1)
-            self.Leav.append(c)
-        self.root = self.Leav[0]
-        self.Buffer = list(range(10))
-        self.codes = []
-        self.huffmanStr = ""
-    #用递归的思想生成编码
+            newNode = Node(value = (self.Leav[-1].value + self.Leav[-2].value))
+            newNode.left = self.Leav.pop(-1)
+            newNode.right = self.Leav.pop(-1)
+            self.Leav.append(newNode)
+    #生成编码表
     def pre(self, tree, length):
         node = tree
         s = ""
         if (not node):
             return
         elif node.name:
-            print (node.name + '    encoding:', end = ''),
             for i in range(length):
-                #print (self.Buffer[i], end = '')
-                s += self.Buffer[i]
-            print (s + '\n')
+                s += self.curCode[i]
             code = (node.name, s)
             self.codes.append(code)
             return
-        self.Buffer[length] = '0'
+        self.curCode[length] = '0'
         self.pre(node.left, length + 1)
-        self.Buffer[length] = '1'
+        self.curCode[length] = '1'
         self.pre(node.right, length + 1)
-    #生成哈夫曼编码   
-    def encodeStr(self, text):
-        for char in text:
+    #生成哈夫曼编码
+    def encodeStr(self):
+        for char in self.text:
             for item in self.codes:
                 if char == item[0]:
                     self.huffmanStr += item[1]
                     break
-        return self.huffmanStr
+    #哈夫曼解码
     def decodeStr(self):
-        orignStr = ''
+        self.originStr = ''
         while self.huffmanStr != '':
             for item in self.codes:
                 if item[1] in self.huffmanStr:
                     if self.huffmanStr.index(item[1]) == 0:
-                        orignStr += item[0]
+                        self.originStr += item[0]
                         self.huffmanStr = self.huffmanStr[len(item[1]):]
                         break
-        return orignStr
-    def get_code(self, text):
+    def get_code(self):
         self.pre(self.root, 0)
         print(self.codes)
-        haf = self.encodeStr(text)
-        print(haf)
-        ori = self.decodeStr()
-        print(ori)
+        self.encodeStr()
+        print(self.huffmanStr)
+        self.decodeStr()
+        print(self.originStr)
 
 if __name__=='__main__':
-    #输入的是字符及其频数
     text = "     wwwwlllleeiiru"
-    #char_weights = [(' ', 5), ('w', 4), ('l', 4), ('e', 2), ('i', 2), ('r',1), ('u', 1)]
-    char_weights = count_freq(text)
-    tree = HuffmanTree(char_weights)
-    tree.get_code(text)
+    tree = HuffmanTree(text)
+    tree.get_code()
