94d772980e6e2d4c68af06e0c437076c3f922a94
baoxin <baoxin@emails.bjut.edu.cn>
2021-12-11 15:21:20 +0800

    文件编码+高亮替换

--- huffmanUI.py
+++ huffmanUI.py
@@ -4,19 +4,31 @@ from tkinter.filedialog import *
 from huffmanTree import *
 
 class MyApp(tk.Frame):
-    
-    def __init__(self):
-        super().__init__()
-        self.pack()
+    def __init__(self, parent = None):
+        tk.Frame.__init__(self, parent)
+        tk.Frame.pack(self)
+        self.grid()
         self.content = ""
         self.setupButton()
     
     def setupButton(self):
-        bt1 = tk.Button(self, text = '打开', command = self.get_file).pack()
-        bt2 = tk.Button(self, text = '编码', command = self.get_huffmanCode).pack()
-        bt3 = tk.Button(self, text = '译码', command = self.get_originCode).pack()
+        bt1 = tk.Button(self, text = '打开', command = self.get_file)
+        bt1.grid(row = 0, column = 0, padx = 5, pady = 5)
+        bt2 = tk.Button(self, text = '编码', command = self.get_huffmanCode)
+        bt2.grid(row = 0, column = 1, padx = 5, pady = 5)
+        bt3 = tk.Button(self, text = '译码', command = self.get_originCode)
+        bt3.grid(row = 0, column = 2, padx = 5, pady = 5)
+        self.serEntry = tk.Entry(self)
+        self.serEntry.grid(row = 2, column = 0, padx = 5, pady = 5)
+        serBtn = tk.Button(self, text = "查找", command = self.search)
+        serBtn.grid(row = 2, column = 1, padx = 5, pady = 5)
+        self.repEntry = tk.Entry(self)
+        self.repEntry.grid(row = 2, column = 2, padx = 5, pady = 5)
+        repBtn = tk.Button(self, text = "替换全部", command = self.replace)
+        repBtn.grid(row = 2, column = 3, padx = 5, pady = 5)
         self.text = tk.Text(self)
-        self.text.pack()
+        self.text.grid(row = 4, column = 0, columnspan = 4, padx = 5, pady = 5)
+        self.text.tag_configure("found", background = "yellow")
 
     def get_file(self):
         # 打开文件
@@ -42,7 +54,7 @@ class MyApp(tk.Frame):
         treeview.heading("字母", text = "字母") # 显示表头
         treeview.heading("出现次数", text = "出现次数")
         treeview.heading("哈夫曼编码", text = "哈夫曼编码")
-        treeview.pack()
+        treeview.grid()
         i = 0
         for item in self.tree.codes: # 写入数据
             treeview.insert('', i, values = (item[0], item[1], item[2]))
@@ -52,6 +64,28 @@ class MyApp(tk.Frame):
         self.text.delete(0.0, tk.END)  # 清空文本框内容
         self.text.insert(tk.END, self.tree.originStr)  # 在光标后插入内容
 
+    def search(self):
+        self.text.tag_remove("found", "1.0", tk.END)
+        start = "1.0"
+        key = self.serEntry.get()
+        if(len(key.strip()) == 0):
+            return
+        while True:
+            pos = self.text.search(key, start, tk.END)
+            if(pos == ""):
+                break
+            self.text.tag_add("found", pos, "%s + %dc" % (pos, len(key)))
+            start = "%s + %dc" % (pos, len(key))
 
-if __name__ == '__main__': 
-    MyApp().mainloop()
+    def replace(self):
+        serKey = self.serEntry.get()
+        repKey = self.repEntry.get()
+        self.text.delete(0.0, tk.END)
+        self.text.insert(tk.END, self.content.replace(serKey, repKey))
+        
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry("600x400") 
+    root.wm_title("文件")
+    MyApp(root)
+    root.mainloop()
